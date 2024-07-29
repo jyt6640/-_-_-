a67d1c59d5ffbd3836e751b6a7a850c1aaff291f
Miku-zzAng <hako5460@naver.com>
2024-07-30 01:22:45 +0900

    vue3 셋팅

---
+++ frontend/vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+    transpileDependencies: true,
+    lintOnSave: false,
+    chainWebpack: (config) => {
+        config.module.rules.delete('eslint');
+    },
+});
